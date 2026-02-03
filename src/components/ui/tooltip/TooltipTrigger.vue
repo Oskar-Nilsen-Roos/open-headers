3f8e10cdd32581d081e8bdc7f58ea2a9af0453da
Oskar Nilsen Roos <oskar.roos@mecenat.com>
2026-02-03 22:37:08 +0100

    Fix TooltipTrigger to forward attrs

--- src/components/ui/tooltip/TooltipTrigger.vue
+++ src/components/ui/tooltip/TooltipTrigger.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import type { TooltipTriggerProps } from "reka-ui"
-import { TooltipTrigger } from "reka-ui"
+import { TooltipTrigger, useForwardProps } from "reka-ui"
 
 const props = defineProps<TooltipTriggerProps>()
+const forwardedProps = useForwardProps(props)
 </script>
 
 <template>
   <TooltipTrigger
     data-slot="tooltip-trigger"
-    v-bind="props"
+    v-bind="forwardedProps"
   >
     <slot />
   </TooltipTrigger>
